cba5266dcc1b0532247be42cff703a83d927cb29
Rahaman <36rahaman@gmail.com>
2023-07-27 20:56:06 +0530

    fff

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      host: env('PGHOST', 'containers-us-west-57.railway.app'),
+      port: env.int('PGPORT', 7794),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'OL70zU7ss1NZ3LZEQ2nE'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
